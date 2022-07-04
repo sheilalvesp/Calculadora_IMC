3fcd614808c9a94d878d81ffdaf1832968f1e5ca
Sheila Alves <102933005+sheilalvesp@users.noreply.github.com>
2022-07-04 17:13:35 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,84 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+
+
+export default function App() {
+  const[peso, setPeso]=useState ('');
+  const[altura,setAltura]=useState('');
+ 
+function handleSubmit(){
+ const alt = altura/100;
+ const imc = peso/(alt *alt);
+ 
+ if(imc < 18.8){
+  alert('Você está abaixo do peso!' + imc.toFixed(2));
+ }else if(imc >= 18.6 && imc <24.9){
+  alert('Peso ideal!' + imc.toFixed(2));
+ }else if(imc >= 24.9 && imc <34.9){
+  alert('Levemente acima do peso!' + imc.toFixed(2));
+ }
+}
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Calcule seu IMC</Text>
+      
+      
+      <TextInput
+      style={styles.input}
+      value={peso}
+      onChangeText={(peso)=>setPeso(peso)}
+      placeholder= "Peso (kg)"
+      keyboardType='numeric'
+      />
+
+      <TextInput 
+      style={styles.input}
+      value={altura}
+      onChangeText={(altura)=>setAltura(altura)}
+      placeholder= "Altura (cm)"
+      keyboardType='numeric'
+      />
+
+      <TouchableOpacity 
+      style={styles.button}
+      onPress={handleSubmit}
+      >
+        <Text style={styles.buttonText}>Calcular</Text>
+      </TouchableOpacity>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  title:{
+    textAlign: 'center',
+    marginTop: 25,
+    fontSize: 30,
+  },
+  input:{
+    backgroundColor:'#708090',
+    borderRadius:10,
+    margin:15,
+    padding: 10,
+    color :'#ffffff',
+    fontSize: 23,
+  },
+  button:{
+    justifyContent:'center',
+    alignItems:'center',
+    margin:15,
+    backgroundColor:"#add8e6",
+    padding:20,
+    borderRadius:10,
+  },
+  buttonText:{
+    color:'#FFF',
+    fontSize: 25,
+  }
+
+});
